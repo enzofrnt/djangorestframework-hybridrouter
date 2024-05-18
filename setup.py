858700947e9b3c6167f5075854a45dd8de5df156
Fournet Enzo <enzofournet31@gmail.com>
2024-05-18 21:20:32 +0200

    0.1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='djangorestframework-hybridrouter',
-    version='0.1.3',
+    version='0.1.4',
     packages=[
         'hybridrouter',
     ],
