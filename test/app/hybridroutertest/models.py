1bfb883a400ddd1de94193dcc530f9d4f2014ea6
Fournet Enzo <enzofournet31@gmail.com>
2024-09-24 17:06:57 +0200

    delete all not needed

--- test/app/hybridroutertest/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Auto(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-    
-# Create a model view set
